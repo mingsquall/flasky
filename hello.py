bf4ff65d555cc064eacc185be207e1a8851e2fcb
YuanMing Liu <774166816@qq.com>
2017-07-26 22:31:47 +0800

    demo

--- hello.py
+++ hello.py
@@ -1,32 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+# from flask.ext.script import Manager
+from flask_moment import Moment
+from flask import url_for
+from datetime import datetime
+import jinja2
 from flask import request
 from flask import abort
 from flask import make_response
-from flask.ext.script import Manager
+
 
 app = Flask(__name__)
+# manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 # 定义路由，使用装饰器把函数注册为路由，index()函数也是根地址的处理程序
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your Browser is %s.</p>' % user_agent
-    # return '<h1>Bad Request</h1>', 200
-    # response = make_response('<h1>This doc carries a cookie!</h1>')
-    # response.set_cookie('answer',42)
-    # return response
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s! </h1>' % name
+    return render_template('user.html', name=name)
 
-# @app.route('/user/<id>')
-# def get_user(id):
-#     user = load_user(id)
-#     if not user:
-#         abort(404)
-#     return '<h1>Hello, %s</h1>' % user.name
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
