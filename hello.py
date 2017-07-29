4ec08fba58bb0f484487aa657db02a1d1671830d
YuanMing Liu <774166816@qq.com>
2017-07-29 22:00:43 +0800

    add WTForm func

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
 from flask_bootstrap import Bootstrap
 # from flask.ext.script import Manager
+from flask_wtf import FlaskForm
 from flask_moment import Moment
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask import url_for
 from datetime import datetime
 import jinja2
@@ -11,15 +14,32 @@ from flask import make_response
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '@!*H(NF(_S!L'
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+# -*- NameForm表单中有一个名为name的文本字段，以及一个名为submit的提交按钮
+class NameForm(FlaskForm):
 
-# 定义路由，使用装饰器把函数注册为路由，index()函数也是根地址的处理程序
-@app.route('/')
+    name = StringField('What is your name?', validators=[DataRequired()]) # StringField类 表示属性为type='text'的<input>元素
+    submit = SubmitField('Submit') # SubmitField类 表示属性为type='submit'的<input>元素
+
+# -*- 定义路由，使用装饰器把函数注册为路由，index()函数也是根地址的处理程序
+# -*- Flask在URL映射中把这个视图函数注册为GET和POST请求的处理程序。如果没指定methods，就只把它注册为GET请求的处理程序。
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    # name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        session['name'] = form.name.data # session['name']存储了同一个会话中的文本框内容
+        form.name.data = ''
+        # name = form.name.data
+        return redirect(url_for('index'))
+
+    # 使用get()获取字典中键对应的值以避免未找到键的异常情况，因为对于不存在的键，get()会返回None
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
